d23bef78496caf707c63567f9ff2fbf73f6f9f61
Rafael Mattia <rdmattia@inf.ufpel.edu.br>
2023-06-25 17:42:33 -0300

    att cart css

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,19 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//     var cartButton = document.getElementById("cart-button");
-//     var cartModal = document.getElementById("cart-modal");
-//     var closeBtn = document.getElementsByClassName("close")[0];
-    
-//     cartButton.addEventListener("click", function() {
-//       cartModal.style.display = "flex";
-//     });
-    
-//     closeBtn.addEventListener("click", function() {
-//       cartModal.style.display = "none";
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function(){
-  var cartHtml = '../pages/cart.html';
-  var indexHtml = '../pages/index.html';
-  var closeButton = cart.html.g
+    var cartButton = document.getElementById("cart-button");
+    var cartModal = document.getElementById("cart-modal");
+    var closeBtn = document.getElementsByClassName("close")[0];
+    
+    cartButton.addEventListener("click", function() {
+      cartModal.style.display = "flex";
+    });
+    
+    closeBtn.addEventListener("click", function() {
+      cartModal.style.display = "none";
+    });
 });
